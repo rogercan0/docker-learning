21535c676eed465338b4b6ac7db2aaae89161945
roger_cano <rogercano97@gmail.com>
2025-11-11 12:41:16 +0100

    Better commented lol

--- my_app.py
+++ my_app.py
@@ -1,4 +1,6 @@
-# This is a simple Python application that prints messages to the console.
+"""
+This is a simple Python application that prints messages to the console.
+"""
 print("Hello from inside a container!")
 print("This is my tiny Python app.")
 print("Bye 👋")
